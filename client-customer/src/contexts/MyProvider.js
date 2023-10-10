e2026888dba5c63f06cb66a0374ce00146dc3135
nhan1505 <92814521+nhan1505@users.noreply.github.com>
2023-10-10 15:14:35 +0700

    Add files via upload

---
+++ client-customer/src/contexts/MyProvider.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import MyContext from './MyContext';
+
+class MyProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { // global state
+      // variables
+      token: '',
+      customer: null,
+      mycart: [],
+
+      // functions
+      setToken: this.setToken,
+      setMycart: this.setMycart,
+      setCustomer: this.setCustomer
+    };
+  }
+  setToken = (value) => {
+    this.setState({ token: value });
+  }
+  setCustomer = (value) => {
+    this.setState({ customer: value });
+  }
+  setMycart = (value) => {
+    this.setState({ mycart: value });
+  }
+  render() {
+    return (
+      <MyContext.Provider value={this.state}>
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+export default MyProvider;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.njgfykk.mongodb.net',
+    DB_USER: 'nhan1505',
+    DB_PASS: '123456nhan',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'caijcxdc',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'nhandinhthanh59@gmail.com', // gmail service
+    EMAIL_PASS: 'sysfqbmfhpxtqlus'
+  };
+  module.exports = MyConstants;
