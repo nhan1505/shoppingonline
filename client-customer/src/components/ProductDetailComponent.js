d12ed480e1563370b5f9b07b6efc021f01719b0b
nhan1505 <92814521+nhan1505@users.noreply.github.com>
2023-10-11 23:19:23 +0700

    Update ProductDetailComponent.js

--- client-customer/src/components/ProductDetailComponent.js
+++ client-customer/src/components/ProductDetailComponent.js
@@ -3,48 +3,85 @@ import React, { Component } from 'react';
 import withRouter from '../utils/withRouter';
 import MyContext from '../contexts/MyContext';
 
-class ProductDetail extends Component {
-  static contextType = MyContext;
 
+class ProductDetail extends Component {
+    static contextType = MyContext;
   constructor(props) {
     super(props);
     this.state = {
       product: null,
       txtQuantity: 1
     };
-
-    // Bind the click event handler
-    this.btnAdd2CartClick = this.btnAdd2CartClick.bind(this);
   }
-
   render() {
     const prod = this.state.product;
-    if (prod !== null) { // Use !== to check for both value and type
+    if (prod != null) {
       return (
-        // ... Your existing JSX
+        <div className="align-center">
+          <h2 className="text-center">PRODUCT DETAILS</h2>
+          <figure className="caption-right">
+            <img src={"data:image/jpg;base64," + prod.image} width="400px" height="400px" alt="" />
+            <figcaption>
+              <form>
+                <table>
+                  <tbody>
+                    <tr>
+                      <td align="right">ID:</td>
+                      <td>{prod._id}</td>
+                    </tr>
+                    <tr>
+                      <td align="right">Name:</td>
+                      <td>{prod.name}</td>
+                    </tr>
+                    <tr>
+                      <td align="right">Price:</td>
+                      <td>{prod.price}</td>
+                    </tr>
+                    <tr>
+                      <td align="right">Category:</td>
+                      <td>{prod.category.name}</td>
+                    </tr>
+                    <tr>
+                      <td align="right">Quantity:</td>
+                      <td><input type="number" min="1" max="99" value={this.state.txtQuantity} onChange={(e) => { this.setState({ txtQuantity: e.target.value }) }} /></td>
+                    </tr>
+                    <tr>
+                     <td></td>
+                     <td><input type="submit" value="ADD TO CART" onClick={(e) => this.btnAdd2CartClick(e)} /></td>
+                     </tr>
+                  </tbody>
+
+                </table>
+              </form>
+            </figcaption>
+          </figure>
+        </div>
       );
     }
-    return <div />;
+    return (<div />);
   }
-
   componentDidMount() {
-    const params = this.props.match.params; // Use this.props.match.params to access route parameters
+    const params = this.props.params;
     this.apiGetProduct(params.id);
   }
-
-  // ... Your other methods
-
-  btnAdd2CartClick(e) {
+  // apis
+  apiGetProduct(id) {
+    axios.get('/api/customer/products/' + id).then((res) => {
+      const result = res.data;
+      this.setState({ product: result });
+    });
+  }
+    btnAdd2CartClick(e) {
     e.preventDefault();
     const product = this.state.product;
-    const quantity = parseInt(this.state.txtQuantity, 10); // Specify base 10 for parseInt
+    const quantity = parseInt(this.state.txtQuantity);
     if (quantity) {
       const mycart = this.context.mycart;
-      const index = mycart.findIndex(x => x.product._id === product._id);
-      if (index === -1) {
+      const index = mycart.findIndex(x => x.product._id === product._id); // check if the _id exists in mycart
+      if (index === -1) { // not found, push newItem
         const newItem = { product: product, quantity: quantity };
         mycart.push(newItem);
-      } else {
+      } else { // increasing the quantity
         mycart[index].quantity += quantity;
       }
       this.context.setMycart(mycart);
@@ -54,5 +91,4 @@ class ProductDetail extends Component {
     }
   }
 }
-
 export default withRouter(ProductDetail);
