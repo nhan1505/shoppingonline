5ed5be836e9fdb113bd570ca2a7b7aad0ecf2096
nhan1505 <92814521+nhan1505@users.noreply.github.com>
2023-10-24 14:57:23 +0700

    Update MenuComponent.js

--- client-customer/src/components/MenuComponent.js
+++ client-customer/src/components/MenuComponent.js
@@ -9,37 +9,43 @@ class Menu extends Component {
     this.state = {
       categories: [],
       txtKeyword: ''
-
     };
   }
+
   render() {
     const cates = this.state.categories.map((item) => {
-      <li key={item._id} className="menu"><Link to=''>{item.name}</Link></li>
       return (
-        <li key={item._id} className="menu"><Link to={'/product/category/' + item._id}>{item.name}</Link></li>);
+        <li key={item._id} className="menu">
+          <Link to={'/product/category/' + item._id}>{item.name}</Link>
+        </li>
+      );
     });
+
     return (
       <div className="border-bottom">
         <div className="float-left">
           <ul className="menu">
-          <li className="menu"><Link to='/'>Home</Link></li>
+            <li className="menu">
+              <Link to='/'>Home</Link>
+            </li>
             {cates}
           </ul>
         </div>
         <div className="float-right">
-       <form className="search">
-        <input type="search" placeholder="Enter keyword" className="keyword" value={this.state.txtKeyword} onChange={(e) => { this.setState({ txtKeyword: e.target.value }) }} />
-        <input type="submit" value="SEARCH" onClick={(e) => this.btnSearchClick(e)} />
-      </form
+          <form className="search">
+            <input type="search" placeholder="Enter keyword" className="keyword" value={this.state.txtKeyword} onChange={(e) => { this.setState({ txtKeyword: e.target.value }) }} />
+            <input type="submit" value="SEARCH" onClick={() => this.btnSearchClick()} />
+          </form>
         </div>
         <div className="float-clear" />
       </div>
-      
     );
   }
+
   componentDidMount() {
     this.apiGetCategories();
   }
+
   // apis
   apiGetCategories() {
     axios.get('/api/customer/categories').then((res) => {
@@ -47,10 +53,10 @@ class Menu extends Component {
       this.setState({ categories: result });
     });
   }
-  btnSearchClick(e) {
-    e.preventDefault();
+
+  btnSearchClick() {
     this.props.navigate('/product/search/' + this.state.txtKeyword);
   }
 }
-export default withRouter(Menu);
 
+export default withRouter(Menu);
